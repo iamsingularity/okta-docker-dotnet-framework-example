0e4959e9729a755995f455c8db69633277fb8ebb
Charlie Holland <chaholl@lineone.net>
2019-09-15 11:33:35 +0100

    Initial checkin

---
+++ CrudMVCCodeFirst/Controllers/LaunchController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CrudMVCCodeFirst.Data;
+using CrudMVCCodeFirst.Models;
+
+namespace CrudMVCCodeFirst.Controllers
+{
+    public class LaunchController : Controller
+    {
+        private LaunchContext db = new LaunchContext();
+
+        // GET: Launch
+        public ActionResult Index()
+        {
+            return View(db.Launches.ToList());
+        }
+
+        // GET: Launch/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            LaunchEntry launchEntry = db.Launches.Find(id);
+            if (launchEntry == null)
+            {
+                return HttpNotFound();
+            }
+            return View(launchEntry);
+        }
+
+        [Authorize]
+        // GET: Launch/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Launch/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize]
+        public ActionResult Create([Bind(Include = "Id,LaunchInfo,PostedByUserName")] LaunchEntry launchEntry)
+        {
+            launchEntry.PostedByUserName = this.User.Identity.Name;
+
+            if (ModelState.IsValid)
+            {
+                db.Launches.Add(launchEntry);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(launchEntry);
+        }
+
+        // GET: Launch/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            LaunchEntry launchEntry = db.Launches.Find(id);
+            if (launchEntry == null)
+            {
+                return HttpNotFound();
+            }
+            return View(launchEntry);
+        }
+
+        // POST: Launch/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,LaunchInfo,PostedByUserName")] LaunchEntry launchEntry)
+        {
+            launchEntry.PostedByUserName = this.User.Identity.Name;
+
+            if (ModelState.IsValid)
+            {
+                db.Entry(launchEntry).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(launchEntry);
+        }
+
+        // GET: Launch/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            LaunchEntry launchEntry = db.Launches.Find(id);
+            if (launchEntry == null)
+            {
+                return HttpNotFound();
+            }
+            return View(launchEntry);
+        }
+
+        // POST: Launch/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            LaunchEntry launchEntry = db.Launches.Find(id);
+            db.Launches.Remove(launchEntry);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ CrudMVCCodeFirst/Data/LaunchContext.cs
@@ -0,0 +1,24 @@
+﻿using CrudMVCCodeFirst.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace CrudMVCCodeFirst.Data
+{
+    public class LaunchContext : DbContext
+    {
+        public LaunchContext() : base("LaunchContext")
+        {
+        }
+
+        public DbSet<LaunchEntry> Launches { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
